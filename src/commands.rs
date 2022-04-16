61c6a9b703171eda9fa112f03064ae20f6d49ecf
Sandip Nath Tiwari <sntw92@gmail.com>
2022-04-16 18:52:58 +0530

    Add documentation for commands

--- src/commands.rs
+++ src/commands.rs
@@ -9,7 +9,7 @@ use crate::chunk::{Chunk, ChunkError};
 use crate::png::{Png,PngError};
 
 #[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version, about)]
 #[clap(propagate_version = true)]
 pub struct Cli {
     #[clap(subcommand)]
@@ -18,33 +18,63 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Encode a secret message into a PNG file
+    /// 
+    /// The secret message is encoded as a non-critical chunk
+    /// inside the PNG file. A single invocation can add a single
+    /// secret-message containing chunks. Multiple invocations
+    /// can be used to add multiple chunks.
     Encode(EncodeArgs),
+    /// Decode the secret message from a PNG file.
+    /// 
+    /// The message is only extracted and displayed. The input file
+    /// is not modified.
     Decode(DecodeArgs),
+    /// Remove the embedded secret message(s) from a PNG file
+    /// 
+    /// A single invocation remove one embedded message chunk.
+    /// If there are multiple embedded messages, multiple
+    /// invocations need to be used even if they have the same
+    /// chunk-type.
     Remove(RemoveArgs),
+    /// Dump all chunks inside the PNG file
+    /// 
+    /// This is useful for debugging. Currently, data is also
+    /// dumped as HEX array. The output is NOT easily parseable
+    /// programmatically. This might be changed in future!
     Print(PrintArgs),
 }
 #[derive(Args, Debug)]
 pub struct EncodeArgs {
+    /// Path to the input PNG file
     input_file_path: String,
+    /// Four byte valid ASCII string for chunk type
     chunk_type_str: String,
+    /// A UTF-8 message string
     message: String,
+    /// Path to the output PNG file. If not specified, input file is used
     output_file_path: Option<String>,
 }
 #[derive(Args, Debug)]
 
 pub struct DecodeArgs {
+    /// Path to the input PNG file
     input_file_path: String,
+    /// Four byte valid ASCII string for chunk type
     chunk_type_str: String,
 }
 #[derive(Args, Debug)]
 
 pub struct RemoveArgs {
+    /// Path to the input PNG file
     input_file_path: String,
+    /// Four byte valid ASCII string for chunk type
     chunk_type_str: String,
 }
 #[derive(Args, Debug)]
 
 pub struct PrintArgs {
+    /// Path to the input PNG file
     input_file_path: String,
 }
 
